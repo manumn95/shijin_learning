10963bdcd5cfe8039f5d4a0e8ec1b89d1076b9d0
manumn95 <manumnofficial.95@gmail.com>
2024-02-16 16:34:13 +0530

    functions

---
+++ day3/Readme.md
@@ -0,0 +1,23 @@
+## Functions
+Introduction to functions
+function & return keyword
+types of function
+
+##Task:
+Do the below programs in anonymous function & IIFE
+Print odd numbers in an array [1,2,3,4,5];
+Convert all the strings to title caps in a string array['apple','mango']
+Sum of all numbers in an array[1,2,3,4]
+Return all the prime numbers in an array
+Return all the palindromes in an array [malayalam,madam,manu]
+Return median of two sorted arrays of the same size.[1,2,3,4][1,2,3,4]
+Remove duplicates from an array[1,1,1,2,3,4]
+Rotate an array by k times[1,2,3,4,5]
+
+
+Do the below programs in arrow functions.
+Print odd numbers in an array
+Convert all the strings to title caps in a string array
+Sum of all numbers in an array
+Return all the prime numbers in an array
+Return all the palindromes in an array
---
+++ day3/index.js
@@ -0,0 +1,74 @@
+//function
+// function hello()
+// {
+//     console.log(5+3);
+// }
+// hello();
+
+//Types
+//1.funtion without arguments and without return value;
+//function hello()
+// {
+//     console.log(5+3);
+// }
+// hello();
+//2.funtion with arguments and with return value
+
+// function sum(a='guest user')//default arguments
+// {
+//     return a;
+// }
+// let result = sum('shijin');
+// console.log(result);
+
+// let a =["apple","orange","mango","kiwi"];
+// let[Apple,orange,...fruits]=a;
+// console.log(fruits);
+
+// 
+
+//Types of function
+
+/*1. Named function
+  2. Anonymous function
+  3.Arrow function
+  4.IIFE function*/
+
+  //Named function
+
+  function sum()
+  {
+    //statement;
+  }
+
+  // Anonymous Function/nameless function
+
+  let sum = function()
+  {
+    //statement;
+
+  }
+
+  //Arrow function 
+
+  let sum = ()=>
+  {
+    //statement;
+  }
+
+  //IIFE function
+  //Anonymous,Arrow
+(function()
+  {
+    //statement;
+
+  })();
+
+  (()=>
+  {
+    //statement;
+  })();
+ 
+  
+
+
