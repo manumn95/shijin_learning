0f2df14fc6898a865aa216b67728a544b872c56e
manumn95 <manumnofficial.95@gmail.com>
2024-02-18 21:58:35 +0530

    day5 added

---
+++ day5/index.js
@@ -0,0 +1,52 @@
+//function prototype
+
+// function person(b=0)
+// {
+
+//     this.getDetails = function()
+//     {
+
+//        console.log(b.age);
+        
+//     }
+   
+//     }
+//     let person1 = new person(25);
+//     console.log(person1);
+
+//class
+
+//Array methods
+//map
+
+// let numbers = [1,2,3,4];
+
+// let result = numbers.map((val)=>{
+// return val*2;
+// })
+
+// console.log(result);
+// console.log(numbers);
+
+//Reduce
+
+// let numbers = [1,2,3,5];
+
+// let val = numbers.reduce((pre,curr)=>{
+//     return pre*curr;
+// },100)
+// console.log(val);
+
+//filter
+
+// let numbers = [1,2,3,4,5,6];
+
+// let f = numbers.filter((val)=>{
+//     return val%2==0;
+// })
+// console.log(f);
+
+
+   
+   
+   
