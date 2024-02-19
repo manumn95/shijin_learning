293e970b322c63cbc63cdba111d296dcb29a9f9c
manumn95 <manumnofficial.95@gmail.com>
2024-02-19 08:51:22 +0530

    day6

---
+++ day6/index.js
@@ -0,0 +1,58 @@
+//class
+
+// let obj = {
+//     name:'',
+//     age:0
+// }
+
+// let obj1 = {
+//     ...obj
+// }
+// obj1.name = 'shijin';
+// obj1.age = 24;
+// console.log(obj1);
+
+// class Book{
+   
+//     //methods
+//     getDetails()
+//     {
+//        console.log(this);
+//     }
+// }
+// let book1 = new Book();
+// let book2 = new Book();
+// // console.log(book2);
+// book1.name = 'shijin';
+// book1.author = 'Berlin';
+// book1.getDetails();
+// // book2.name = 'manu';
+// // book2.author = 'abc';
+// // console.log(book2);
+
+
+// class person{
+//     static totalVotes =0;
+//    vote = 0;
+//    addVote(){
+//     this.vote++;
+//     person.totalVotes++;
+//    }
+//    voteStatus()
+//    {
+//     console.log(`Total Votes: ${person.totalVotes}`);
+//     console.log(`Votes: ${this.vote}`);
+//    }
+// }
+
+// let person1 = new person();
+// person1.addVote();
+// person1.addVote();
+// person1.voteStatus();
+// let person2 = new person();
+// person2.addVote();
+// person2.voteStatus();
+
+//------------------------------------------------------------------
+
+
